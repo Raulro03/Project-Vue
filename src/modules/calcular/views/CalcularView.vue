fb8809c9646bc74bfb12e86002851cf06518a750
Raulro03 <raulromeraortiz@gmail.com>
2025-02-18 10:46:56 +0100

    Hacemos un nuevo modulo para calcular multiplicando por 3

---
+++ src/modules/calcular/views/CalcularView.vue
@@ -0,0 +1,21 @@
+<template>
+
+        <h3>Propiedad Computadas</h3>
+        <input type="text" v-model="valor">
+        <p>Valor: {{ valorTriple}}</p>
+        <ResultadoSection :resultado="valorTriple"></ResultadoSection>
+</template>
+
+<script setup>
+    import { ref, computed } from 'vue';
+    import ResultadoSection from '../components/ResultadoSection.vue';
+    const valor = ref('');
+
+    const valorTriple = computed(() => {
+        return valor.value * 3;
+    });
+</script>
+
+<style scoped>
+
+</style>
