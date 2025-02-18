5abbf44a6d70a211f21f5ccf2de804ae7807c486
Raulro03 <raulromeraortiz@gmail.com>
2025-02-18 11:07:37 +0100

    Usar v-else v-if y v-else-if y funcion mostrar elemento si es mayor o igual a 10

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -3,7 +3,9 @@
         <h3>Propiedad Computadas</h3>
         <input type="text" v-model="valor">
         <p>Valor: {{ valorTriple}}</p>
-        <ResultadoSection :resultado="valorTriple"></ResultadoSection>
+        <ResultadoSection v-if="mostrarElemento" :resultado="valorTriple"></ResultadoSection>
+        <div v-else-if="isNaN(valor)">No es valido, debe ser un numero</div>
+        <div v-else>Aqui no se muestra nada</div>
 </template>
 
 <script setup>
@@ -13,6 +15,10 @@
 
     const valorTriple = computed(() => {
         return valor.value * 3;
+    });
+
+    const mostrarElemento = computed(() => {
+        return valorTriple.value >= 10;
     });
 </script>
 
