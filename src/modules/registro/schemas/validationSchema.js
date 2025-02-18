ad7faa66c096c09db725b6b6dd64de123d8ec797
Raulro03 <raulromeraortiz@gmail.com>
2025-02-18 11:15:58 +0100

    Añadir regex a el registro de usuario

--- src/modules/registro/schemas/validationSchema.js
+++ src/modules/registro/schemas/validationSchema.js
@@ -1,7 +1,8 @@
 import * as yup from 'yup';
 
+
 export const schema = yup.object({
-    nombre: yup.string().required('El nombre es requerido'),
+    nombre: yup.string().required('El nombre es requerido').matches(/^[a-zA-Z]+$/, "El nombre de usuario solo puede contener letras"),
     email: yup.string().email('El email no es válido').required('El email es requerido')
 });
 
